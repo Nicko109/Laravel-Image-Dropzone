a67e0d145befc5ea56f040fc91f5c2a3c82ecbfa
Алиса <nkaragodov@inbox.ru>
2023-09-14 21:15:27 +0300

    1) Основа для приложения, миграции, библиотеки 2) Установка dropzone js и прогрузка первых изображений

---
+++ resources/js/components/Index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="w-25">
+<div ref="dropzone" class="d-block btn p-5 bg-dark text-center text-light">
+Upload
+</div>
+    </div>
+</template>
+
+<script>
+import Dropzone from 'dropzone'
+
+export default {
+    name: "Index",
+
+    data() {
+        return {
+            dropzone: null
+        }
+    },
+
+    mounted() {
+        this.dropzone = new Dropzone(this.$refs.dropzone, {
+            url: "/file/post"
+        })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
