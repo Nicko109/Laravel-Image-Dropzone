903a0156011ea79ed2ad26a477d832bdba68495e
Алиса <nkaragodov@inbox.ru>
2023-09-15 11:23:37 +0300

    Публикация изображений в Storage и привязка к постам

--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -4,16 +4,35 @@ namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Post\StoreRequest;
+use App\Models\Image;
 use App\Models\Post;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Storage;
 
 class StoreController extends Controller
 {
-   public function __invoke()
+   public function __invoke(StoreRequest $request)
    {
-       dump (\request()->all());
-       return response(['message' => 'ok']);
+
+       $data = $request->validated();
+       $images = $data['images'];
+       unset($data['images']);
+       $post = Post::firstOrCreate($data);
+       foreach ($images as $image) {
+           $name = md5(Carbon::now() . '_' . $image->getClientOriginalName()) . '.' .$image->getClientOriginalExtension();
+       $filePath = Storage::disk('public')->putFileAs('/images', $image, $name);
+
+       Image::create([
+           'path' => $filePath,
+           'url' => url('/storage/' . $filePath),
+           'post_id' => $post->id
+       ]);
+
+       }
+
+       return response()->json(['message' => 'success']);
    }
 }
